b12dd14e43b75ab583156011e51e1dd943ebb81d
jefflin <linjef0711@gmail.com>
2021-01-14 19:37:35 +0800

    feat: add user registration feature

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -2,13 +2,43 @@
 const express = require('express')
 const { readyState } = require('../../config/mongoose')
 const router = express.Router()
+const User = require('../../models/user')
 
 router.get('/login', (req, res) => {
   res.render('login')
 })
 
+router.post('/login', (req, res) => {
+
+})
+
 router.get('/register', (req, res) => {
   res.render('register')
+})
+
+//
+router.post('/register', (req, res) => {
+  const { name, email, password, confirmPassword } = req.body
+  User.findOne({ email })
+    .then(user => {
+      if (user) {
+        console.log('User already exists.')
+        return res.render('register', {
+          name,
+          email,
+          password,
+          confirmPassword
+        })
+      }
+
+      User.create({
+        name,
+        email,
+        password
+      })
+        .then(() => res.redirect('/'))
+        .catch(err => console.log(err))
+    })
 })
 
 //匯出路由器
