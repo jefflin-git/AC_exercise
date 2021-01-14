87b32566e2d409d1c4cdf2c9989239a205967265
jefflin <linjef0711@gmail.com>
2021-01-14 21:28:42 +0800

    feat: add user authentication in home,create,sort,search,restaurant routes

--- routes/index.js
+++ routes/index.js
@@ -12,18 +12,23 @@ const search = require('./modules/search.js')
 const sort = require('./modules/sort.js')
 //引入create模組程式碼
 const create = require('./modules/create.js')
+//引入users模組程式碼
 const users = require('./modules/users')
+const { authenticator } = require('../middleware/auth')
 
-//將網址結構符合 / 字串的request導向home模組
-router.use('/', home)
+
 //將網址結構符合 /restaurants字串開頭的request導向restaurants模組
-router.use('/restaurants', restaurants)
+router.use('/restaurants', authenticator, restaurants)
 //將網址結構符合 /search字串開頭的request導向search模組
-router.use('/search', search)
+router.use('/search', authenticator, search)
 //將網址結構符合 /sort字串開頭的request導向search模組
-router.use('/sort', sort)
+router.use('/sort', authenticator, sort)
 //將網址結構符合 /new字串開頭的request導向search模組
-router.use('/create', create)
+router.use('/create', authenticator, create)
+//將網址結構符合 /users字串開頭的request導向search模組
 router.use('/users', users)
+//將網址結構符合 / 字串的request導向home模組
+router.use('/', authenticator, home)
+
 //匯出路由器
 module.exports = router
