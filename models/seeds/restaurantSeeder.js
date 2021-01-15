64129c328e06efaa03d5af874d80d75378f3059e
jefflin <linjef0711@gmail.com>
2021-01-15 13:49:45 +0800

    feat: modify restaurantSeeder

--- models/seeds/restaurantSeeder.js
+++ models/seeds/restaurantSeeder.js
@@ -1,14 +1,58 @@
+const bcrypt = require('bcryptjs')
+
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
 //引入mongoose模組擋案
 const db = require('../../config/mongoose.js')
 //載入model
 const RestList = require('../restaurant.js')
-const restaurantList = require('../../restaurant.json')
+const User = require('../user')
+const restaurantList = require('../../restaurant.json').results
+
+const SEED_USERS = [
+  {
+    email: 'user1@example.com',
+    password: '12345678'
+  },
+  {
+    email: 'user2@example.com',
+    password: '12345678'
+  }
+]
 
 // 連線成功
 db.once('open', () => {
   console.log('mongodb connected!')
-  for (let i = 0; i < restaurantList.results.length; i++) {
-    RestList.create(restaurantList.results[i])
-  }
-  console.log('done!')
+  SEED_USERS.forEach((user, index) => {
+    bcrypt
+      .genSalt(10)
+      .then(salt => bcrypt.hash(user.password, salt))
+      .then(hash => User.create({ email: user.email, password: hash }))
+      .then(async (user) => {
+        switch (index) {
+          case 0:
+            const restaurantList1 = []
+            for (let i = 0; i <= 2; i++) {
+              restaurantList[i].userId = user._id
+              restaurantList1.push(restaurantList[i])
+            }
+            await RestList.insertMany(restaurantList1)
+            break
+          case 1:
+            const restaurantList2 = []
+            for (let i = 3; i <= 5; i++) {
+              restaurantList[i].userId = user._id
+              restaurantList2.push(restaurantList[i])
+            }
+            await RestList.insertMany(restaurantList2)
+            break
+        }
+      })
+      .then(() => {
+        console.log('done!')
+        process.exit()
+      })
+  })
 })
